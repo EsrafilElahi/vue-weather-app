4b8fe701a0ba27108e4f0d5eb789d8d1a5a79a89
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-25 00:45:38 +0330

    add some packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VueDevTools()
+    VueDevTools(),
+    tailwindcss()
   ],
 })
