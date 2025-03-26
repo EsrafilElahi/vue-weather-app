972400148eb3f33c450fe32dd2ae7a164fc48ed2
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-26 18:46:04 +0330

    add blue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="min-h-screen bg-gray-100 py-8">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h1 class="text-4xl font-bold text-lighter-gray text-center !mb-2">
+      <h1 class="text-4xl font-bold text-primary-blue text-center !mb-2">
         پیش‌بینی آب و هوا
       </h1>
     </div>
