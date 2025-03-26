3fcf1d318acb0934b20b8fa52d1107f32fa83f82
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-26 22:53:26 +0330

    complete Header and Modal component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import Modal from "@/components/Modal.vue";
+
+const TITLE = "ESRAFIL ELAHI";
+const TEXT = "Senior Frontend Engineer at Okala";
+
+// for multiple model
+// const dialogOpen2 = ref(false);
+const dialogOpen = ref(false);
+</script>
 
 <template>
-  <VAppBar color="primary-blue" density="comfortable">
+  <VAppBar elevation="2" density="comfortable">
     <VAppBarTitle class="text-left">Application Bar</VAppBarTitle>
 
     <template v-slot:append>
       <VIcon
+        @click="dialogOpen = true"
         color="info"
         class="mr-3 cursor-pointer"
         icon="mdi-information"
       ></VIcon>
     </template>
+
+    <!-- for multiple model -->
+    <!-- <Modal v-model:dialogOpen="dialogOpen" v-model:dialogOpen2="dialogOpen2" /> -->
+    <Modal v-model:dialogOpen="dialogOpen" :title="TITLE" :text="TEXT" />
   </VAppBar>
 </template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineModel } from "vue";
+
+interface Props {
+  title: string;
+  text: string;
+}
+
+// for multiple model
+// const dialogOpen2 = defineModel("dialogOpen2");
+const dialogOpen = defineModel<boolean>("dialogOpen");
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="text-center pa-4">
+    <VDialog v-model="dialogOpen" max-width="400">
+      <VCard :text="text" :title="title">
+        <template #actions>
+          <VBtn @click="dialogOpen = false"> OK </VBtn>
+        </template>
+      </VCard>
+    </VDialog>
+  </div>
+</template>
