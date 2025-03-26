068e164caa892ef048bfaea377a660cc00697ae5
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-26 21:24:18 +0330

    remove comment

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,7 +2,6 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import { createVuetify, type ThemeDefinition } from "vuetify";
-// import '@mdi/font/css/materialdesignicons.css'
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
