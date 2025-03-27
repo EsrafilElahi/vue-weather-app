1372566d541aa8e0803ff5a312a07169dec8969b
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-28 01:40:30 +0330

    add savedCities

---
+++ src/components/StoredCityItem.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { watch, watchEffect } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const props = defineProps(["cityItem"]);
+
+const redirectToCityDetailsPage = () => {
+  router.push({
+    name: "cityDetailsPage",
+    params: { city: props.cityItem.name },
+  });
+};
+
+// Use watch instead of watchEffect for specific prop observation
+// watch(
+//   () => props.cityItem,
+//   (newVal) => {
+//     if (newVal) {
+//       console.log('cityItem:', newVal);
+//     }
+//   },
+//   { immediate: true } // need for run first time
+// );
+</script>
+
+<template>
+  <VCard
+    @click="redirectToCityDetailsPage"
+    density="compact"
+    class="d-flex justify-between items-center !p-2 rounded-lg"
+    :aria-label="`Location: ${cityItem.name}`"
+    link
+  >
+    <VRow align="center">
+      <VCol cols="11">
+        <VCardTitle class="!p-0 !text-lg">{{ cityItem.name }}</VCardTitle>
+        <VCardText class="!p-0 !pt-1 !text-md">{{
+          cityItem.sys.country
+        }}</VCardText>
+      </VCol>
+      <VCol cols="1" class="!flex justify-end">
+        <v-icon
+          icon="mdi-chevron-right"
+          color="grey-darken-1"
+          class="ml-2"
+        ></v-icon>
+      </VCol>
+    </VRow>
+  </VCard>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { fetchCity } from "@/apis/fetchCity";
 import FoundCityItem from "@/components/FoundCityItem.vue";
+import StoredCityItem from "@/components/StoredCityItem.vue";
 import { useDebounce } from "@/composables/useDebounce";
-import { onBeforeUnmount, ref } from "vue";
+import { getStoredCities } from "@/lib/utils";
+import { onBeforeUnmount, ref, watchEffect } from "vue";
 
 const cities = ref<any | null>(null);
 let abortController: AbortController | null = null;
+
+const storedCities = getStoredCities();
 
 const debouncedValue = useDebounce(async (val: string) => {
   // Abort previous request
@@ -16,10 +20,14 @@ const debouncedValue = useDebounce(async (val: string) => {
   // Create new controller for this request
   abortController = new AbortController();
 
-  const citiesFound = await fetchCity(val, {
-    signal: abortController.signal,
-  });
-  cities.value = citiesFound;
+  if (val.trim().length > 1) {
+    const results = await fetchCity(val, {
+      signal: abortController.signal,
+    });
+    cities.value = results;
+  } else {
+    cities.value = null; // Reset when search is cleared
+  }
 }, 600);
 
 const handleInput = (e: Event) => {
@@ -31,6 +39,13 @@ onBeforeUnmount(() => {
   if (abortController) {
     abortController.abort();
   }
+});
+
+watchEffect(() => {
+  console.log(
+    "cities !== null && cities?.length > 0 :",
+    cities.value !== null && cities.value.length > 0
+  );
 });
 </script>
 
@@ -48,8 +63,20 @@ onBeforeUnmount(() => {
     >
       <FoundCityItem v-for="city in cities" :key="city.id" :cityItem="city" />
     </div>
-    <VCard v-else-if="cities !== null && cities?.length === 0">
+    <VCard v-else-if="cities !== null && cities?.length === 0" class="mt-1">
       <VCardText class="text-error-red">No City Found</VCardText>
     </VCard>
+
+    <div
+      v-else-if="storedCities.length > 0"
+      :class="{ 'flex flex-col gap-2 mt-10': 'storedCities.length > 0' }"
+    >
+      <h2 class="text-2xl text-text-gray">saved cities</h2>
+      <StoredCityItem
+        v-for="storedCity in storedCities"
+        :key="storedCity.id"
+        :cityItem="storedCity"
+      />
+    </div>
   </VContainer>
 </template>
