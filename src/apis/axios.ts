78ad6cccc63fe2ad47aa383d1610c06bafa02075
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-27 19:09:19 +0330

    add base url dynamically

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,5 @@
 import axios, { type AxiosInstance } from 'axios';
 
-const backendBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL
-
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: backendBaseUrl,
   headers: { 'Content-Type': 'application/json' },
 });
--- src/apis/fetchCity.ts
+++ src/apis/fetchCity.ts
@@ -1,5 +1,7 @@
 import { axiosInstance } from "./axios";
 
+const fetchCityBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL
+
 export const fetchCity = async (query: string) => {
   try {
     const res = await axiosInstance.get(`/${encodeURIComponent(query)}.json`, {
@@ -7,6 +9,7 @@ export const fetchCity = async (query: string) => {
         access_token: import.meta.env.VITE_WEATHER_ACCESS_TOKEN,
         types: 'place',
       },
+      baseURL: fetchCityBaseUrl,
     });
     return res.data.features
   } catch (error) {
