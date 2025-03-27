ac16683e54374946888ddc6e914e8d8b3f667bc7
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-27 19:19:07 +0330

    complete fetch weather

--- src/apis/fetchWeather.ts
+++ src/apis/fetchWeather.ts
@@ -2,9 +2,7 @@ import { axiosInstance } from "./axios";
 
 const fetchWeatherCityBaseUrl = import.meta.env.VITE_FETCH_WEATHER_CITY_BASE_URL
 
-// https://api.openweathermap.org/data/2.5/weather?q=tehran&units=metric&APPID=2af0224f3425ab835e0d63cf9afdccc2
-
-export const fetchCity = async (query: string) => {
+export const fetchWeatherCity = async (query: string) => {
   try {
     const res = await axiosInstance.get(`/weather`, {
       baseURL: fetchWeatherCityBaseUrl,
@@ -14,7 +12,6 @@ export const fetchCity = async (query: string) => {
         APPID: import.meta.env.VITE_WEATHER_APPID,
       },
     });
-    console.log('res weather :', res)
     return res.data
   } catch (error) {
     console.log("err :", error);
