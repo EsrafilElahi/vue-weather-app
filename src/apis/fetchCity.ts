5e391bac7695dd32ebde4c5af15d9749a72d95af
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-27 19:14:32 +0330

    add env and config it

--- src/apis/fetchCity.ts
+++ src/apis/fetchCity.ts
@@ -5,11 +5,11 @@ const fetchCityBaseUrl = import.meta.env.VITE_FETCH_CITY_BASE_URL
 export const fetchCity = async (query: string) => {
   try {
     const res = await axiosInstance.get(`/${encodeURIComponent(query)}.json`, {
+      baseURL: fetchCityBaseUrl,
       params: {
-        access_token: import.meta.env.VITE_WEATHER_ACCESS_TOKEN,
+        access_token: import.meta.env.VITE_CITY_ACCESS_TOKEN,
         types: 'place',
       },
-      baseURL: fetchCityBaseUrl,
     });
     return res.data.features
   } catch (error) {
--- src/apis/fetchWeather.ts
+++ src/apis/fetchWeather.ts
@@ -2,16 +2,20 @@ import { axiosInstance } from "./axios";
 
 const fetchWeatherCityBaseUrl = import.meta.env.VITE_FETCH_WEATHER_CITY_BASE_URL
 
+// https://api.openweathermap.org/data/2.5/weather?q=tehran&units=metric&APPID=2af0224f3425ab835e0d63cf9afdccc2
+
 export const fetchCity = async (query: string) => {
   try {
-    const res = await axiosInstance.get(`/${encodeURIComponent(query)}.json`, {
-      params: {
-        access_token: import.meta.env.VITE_WEATHER_ACCESS_TOKEN,
-        types: 'place',
-      },
+    const res = await axiosInstance.get(`/weather`, {
       baseURL: fetchWeatherCityBaseUrl,
+      params: {
+        q: query,
+        units: 'metric',
+        APPID: import.meta.env.VITE_WEATHER_APPID,
+      },
     });
-    return res.data.features
+    console.log('res weather :', res)
+    return res.data
   } catch (error) {
     console.log("err :", error);
   }
