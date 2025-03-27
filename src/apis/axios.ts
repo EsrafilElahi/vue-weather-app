f68dfd09e609b3351288a257fc337575990343c3
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-27 19:06:20 +0330

    change params city search

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -4,9 +4,5 @@ const backendBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: backendBaseUrl,
-  params: {
-    access_token: import.meta.env.VITE_WEATHER_ACCESS_TOKEN,
-    types: 'place',
-  },
   headers: { 'Content-Type': 'application/json' },
 });
--- src/apis/fetchCity.ts
+++ src/apis/fetchCity.ts
@@ -2,7 +2,12 @@ import { axiosInstance } from "./axios";
 
 export const fetchCity = async (query: string) => {
   try {
-    const res = await axiosInstance.get(`/${encodeURIComponent(query)}.json`);
+    const res = await axiosInstance.get(`/${encodeURIComponent(query)}.json`, {
+      params: {
+        access_token: import.meta.env.VITE_WEATHER_ACCESS_TOKEN,
+        types: 'place',
+      },
+    });
     return res.data.features
   } catch (error) {
     console.log("err :", error);
