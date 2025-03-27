5cbf661f6c39ac58b6d16e329bb779296e1c7b7a
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-27 18:11:51 +0330

    add city Item

---
+++ src/components/FoundCityItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = defineProps(["cityItem"]);
+</script>
+
+<template>
+  <VCard
+    density="compact"
+    class="d-flex justify-between items-center !p-2 rounded-lg"
+    link
+  >
+    <VRow align="center">
+      <VCol cols="11">
+        <VCardTitle class="!p-0 !text-lg">{{ cityItem.text }}</VCardTitle>
+        <VCardText class="!p-0 !pt-1 !text-md">{{
+          cityItem.place_name
+        }}</VCardText>
+      </VCol>
+      <VCol cols="1" class="!flex justify-end">
+        <v-icon
+          icon="mdi-chevron-right"
+          color="grey-darken-1"
+          class="ml-2"
+        ></v-icon>
+      </VCol>
+    </VRow>
+  </VCard>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,27 +1,35 @@
 <script setup lang="ts">
+import { fetchCity } from "@/apis/fetchCity";
+import FoundCityItem from "@/components/FoundCityItem.vue";
 import { useDebounce } from "@/composables/useDebounce";
-import { ref, watch } from "vue";
+import { ref } from "vue";
 
-const searchValue = ref("");
+const cities = ref<any>([]);
 
-const debouncedValue = useDebounce((val: string) => {
-  console.log("val :", val);
+const debouncedValue = useDebounce(async (val: string) => {
+  const citiesFound = await fetchCity(val);
+  cities.value = citiesFound;
 }, 600);
 
 const handleInput = (e: Event) => {
   const target = e.target as HTMLInputElement;
   debouncedValue(target.value);
 };
-
 </script>
 
 <template>
   <VContainer class="mt-3">
     <VTextField
-      v-model="searchValue"
       @input="handleInput"
       append-inner-icon="mdi-magnify"
       label="Search for cities..."
     />
+
+    <div
+      v-if="cities.length > 0"
+      :class="{ 'flex flex-col gap-2 mt-1': 'cities.length > 0' }"
+    >
+      <FoundCityItem v-for="city in cities" :key="city.id" :cityItem="city" />
+    </div>
   </VContainer>
 </template>
