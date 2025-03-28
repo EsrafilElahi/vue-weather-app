66c932a274669765019fa0e26ab9bdbbd1577e0d
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-28 20:34:23 +0330

    adding styles

---
+++ src/components/WeatherDetailCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <VRow
+    class="flex flex-col items-start justify-center gap-1 bg-white !py-[10px] !px-[12px] rounded-lg shadow-md"
+  >
+    <VRow class="flex justify-between items-center gap-2">
+      <span>icon</span>
+      <span v-capitalize class="font-semibold">wind</span>
+    </VRow>
+
+    <span class="text-2xl font-extrabold">12 km/h</span>
+    <span v-capitalize class="text-text-gray">clear conditions</span>
+  </VRow>
+</template>
--- src/views/CityDetailsPage.vue
+++ src/views/CityDetailsPage.vue
@@ -4,6 +4,7 @@ import { getStoredCities } from "@/lib/utils";
 import { onBeforeUnmount, onMounted, ref, watch } from "vue";
 import { useRoute, type RouteLocationNormalized } from "vue-router";
 import { useToast } from "vue-toast-notification";
+import WeatherDetailCard from "@/components/WeatherDetailCard.vue";
 
 interface Params {
   city: string;
@@ -86,5 +87,38 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div>City Details Page</div>
+  <VContainer class="flex flex-col gap-[2rem] !p-0 !mt-[2rem]">
+    <VRow class="bg-white !p-4 rounded-lg shadow-md">
+      <VCol cols="9" class="flex flex-col gap-5">
+        <VRow>
+          <VCol cols="12">
+            <p class="text-2xl font-bold">new york</p>
+          </VCol>
+
+          <VCol cols="12">
+            <p class="text-text-gray">wendnesday</p>
+          </VCol>
+        </VRow>
+
+        <VRow class="flex gap-4">
+          <p class="text-3xl font-bold">23C</p>
+          <p>partly cloudy</p>
+        </VRow>
+      </VCol>
+
+      <VCol cols="3" class="flex flex-col justify-between items-end">
+        <p>add to favorite</p>
+        <p>icon weather</p>
+      </VCol>
+    </VRow>
+
+    <VRow class="!grid grid-cols-1 md:!grid-cols-2 lg:!grid-cols-3 gap-4">
+      <WeatherDetailCard />
+      <WeatherDetailCard />
+      <WeatherDetailCard />
+      <WeatherDetailCard />
+      <WeatherDetailCard />
+      <WeatherDetailCard />
+    </VRow>
+  </VContainer>
 </template>
