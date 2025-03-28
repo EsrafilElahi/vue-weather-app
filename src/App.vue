727fd3c28b1f6241bfa04bf8d0a17122b6000adb
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-28 20:20:39 +0330

    add p

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterView } from "vue-router";
 
 <template>
   <VApp>
-    <div class="min-h-screen bg-gray-100 pt-14">
+    <div class="min-h-screen bg-gray-100 pt-14 px-5 lg:!px-0">
       <Header />
 
       <RouterView />
