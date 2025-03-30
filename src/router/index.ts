881957bcbd14a22e6ee7aa8dfd9c30f8a2987d14
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-31 00:16:32 +0330

    add store.restored in route.ts file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory, type Router } from "vue-router";
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalizedGeneric, type Router } from "vue-router";
 import CityDetailsPage from "@/views/CityDetailsPage.vue";
 import HomePage from "@/views/HomePage.vue";
+import store from "@/store";
 
 export const router: Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,11 +29,17 @@ export const router: Router = createRouter({
   ],
 });
 
+const waitForStorageToBeReady = async (to: RouteLocationNormalizedGeneric, from: RouteLocationNormalizedGeneric, next: NavigationGuardNext) => {
+  await (store as any)?.restored
+  next()
+}
+
 router.beforeEach((to, from, next) => {
   const title = to.params.city || to.meta.title
 
   if (document?.title) {
     window.document.title = `Weather App | ${title}`;
   }
+  waitForStorageToBeReady(to, from, next)
   next();
 })
