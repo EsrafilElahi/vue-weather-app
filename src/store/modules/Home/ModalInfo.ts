7b40197fe3bdd72678297c8ef799224c911e61cd
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-30 22:12:02 +0330

    add modal info module nested

---
+++ src/store/modules/Home/ModalInfo.ts
@@ -0,0 +1,34 @@
+
+// type
+
+interface State {
+  title: string;
+  info: string;
+}
+
+// state
+const state: State = {
+  title: 'ESRAFIL ELAHI',
+  info: 'Senior Frontend Engineer at Okala'
+}
+
+// getters
+const getters = {
+  getInfoComplete: (state: State, getters: any, rootState: any) => {
+    return state
+  }
+}
+
+// mutations
+const mutations = {}
+
+// actions
+const actions = {}
+
+export default {
+  namespace: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
